ea1e3e59f19c6a9a85a6f7fa70d4cfebd0986a05
Raka Hadhyana <13516099@std.stei.itb.ac.id>
2019-02-24 15:13:38 +0700

    add queue schema

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ Doctor = require('./models/doctor');
 mongoose.connect('mongodb://raka:raka24@ds349175.mlab.com:49175/qtime');
 var db = mongoose.connection;
 
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
     res.send('Please use /api/doctors or /api/queues');
 });
@@ -19,7 +21,7 @@ app.get('/api/doctors', function(req, res){
         }
         res.json({doctors: doctors});
     });
-})
+});
 
 app.get('/api/doctors/:_id', function(req, res){
     Doctor.getDoctorById(req.params._id, function(err, doctor){
@@ -28,7 +30,17 @@ app.get('/api/doctors/:_id', function(req, res){
         }
         res.json({doctor: doctor});
     });
-})
+});
+
+app.post('/api/queue', function(req, res){
+    var queue = req.body;
+    Queue.addQueue(queue, function(err, queue){
+        if(err){
+            throw err;
+        }
+        res.json(queue);
+    });
+});
 
 app.listen(process.env.PORT || 3000);
 console.log('Running on port 3000');
---
+++ models/queue.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+
+//Queue Schema
+var queueSchema = mongoose.Schema({
+    user:{
+        type: String,
+        require: true
+    },
+    doctor:{
+        type: Schema.Types.ObjectId,
+        require: true
+    },
+    take_time:{
+        type: Date,
+        default: Date.now
+    },
+    done:{
+        type: Boolean,
+        default: false
+    }
+})
+
+var Queue = module.exports = mongoose.model('Queue', queueSchema);
+
+module.exports.addQueue = function(queue, callback){
+    Queue.create(queue, callback);
+}
